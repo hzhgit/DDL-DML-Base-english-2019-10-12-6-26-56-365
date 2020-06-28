2e6a0285646cd024607e2b05a93fd5935d0154c7
zach-he <971499086@qq.com>
2020-06-28 17:17:32 +0800

    modified by zach

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,46 @@
 -- **Database level:**
 -- Display all the database
+show databases;
 
 -- Enter a certain database
+use student_examination_sys;
 
 -- Create a database
+create database ddl_train;
 
 -- Create the database of the designated character set
+create database ddl_train_2 default character set = 'utf8';
 
 -- Display the creation information fo the database
+show create database ddl_train_2;
 
 -- Revise the codes of the database
+set character_set_database = utf8;
 
 -- Delete a database
+drop database ddl_train;
 
 -- **Table level**
 -- Revise table name
+alter table student rename students;
 
 -- Revise the field's data type
+alter table students modify name varchar(255);
 
 -- Revise field name
+alter table students change name snmae varchar(20);
 
 -- Add field
+alter table students add height FLOAT(3,1);
 
 -- Delete field
+alter table students drop age;
 
 -- Revise the table's storage engine
+alter table students engine = INNODB;
 
 -- Delete the table's foreign key restriant
+alter table students drop foreign key foreignKeyName;
 
 -- Delete a table
+drop table students;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
 -- Insert record
+insert into student values('001','Colin','18','male'),('002','Emma','20','female');
 
 -- Revise record
+update student set age = 23 where id = '002';
 
 --  Delete record
+delete from student where id = '001';
 
 -- Search record
+select * from student;
